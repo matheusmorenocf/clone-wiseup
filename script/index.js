035ae027bae1e7702381e747c97740339a15e323
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-03-05 00:57:23 -0300

    atualizaçao

--- script/index.js
+++ script/index.js
@@ -1,5 +1,12 @@
+//Menu Hamburger
 const checkBox = document.querySelector("#checkbox-menu");
 const navBar = document.querySelector("#navBar");
+//Carrosel
+const carousel = document.querySelector('.carousel');
+const cardsContainer = carousel.querySelector('.container-card-2');
+const cards = cardsContainer.querySelectorAll('.card-2');
+const leftButton = carousel.querySelector('.carousel-prev');
+const rightButton = carousel.querySelector('.carousel-next');
 
 
 checkBox.addEventListener("change", function(){
@@ -12,5 +19,22 @@ checkBox.addEventListener("change", function(){
   }
 })
 
+let currentIndex = 0;
 
+
+leftButton.addEventListener('click', () => {
+  console.log("Previous")
+  if (currentIndex > 0) {
+    currentIndex--;
+    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 20)}px)`;
+  }
+});
+
+rightButton.addEventListener('click', () => {
+  console.log("Next")
+  if (currentIndex < cards.length) {
+    currentIndex++;
+    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 20)}px)`;
+  }
+});
 
