34d6cf7232a25bc98ff7aa2696021a9941d19ed1
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-03-05 01:14:06 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -20,21 +20,37 @@ checkBox.addEventListener("change", function(){
 })
 
 let currentIndex = 0;
+const numVisibleCards = () => {
+  const viewportWidth = window.innerWidth
+  if (viewportWidth > 999 ){
+    return 4
+  }
+  if (viewportWidth <= 999 && viewportWidth > 768){
+    return 3
+  }
+  if (viewportWidth <= 768 && viewportWidth > 420){
+    return 2
+  }
+  if (viewportWidth <= 420){
+    return 1
+  }
+} ;
+
 
 
 leftButton.addEventListener('click', () => {
   console.log("Previous")
   if (currentIndex > 0) {
     currentIndex--;
-    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 20)}px)`;
+    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth)}px)`;
   }
 });
 
 rightButton.addEventListener('click', () => {
   console.log("Next")
-  if (currentIndex < cards.length) {
+  if (currentIndex < cards.length - numVisibleCards()) {
     currentIndex++;
-    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 20)}px)`;
+    cardsContainer.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 25)}px)`;
   }
 });
 
